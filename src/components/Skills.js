087229277e8f943734524b2ff9f8ebbdb621e4cc
avidler <a.vidler@live.co.uk>
2022-04-24 22:21:43 +0100

    first commit

---
+++ src/components/Skills.js
@@ -0,0 +1,35 @@
+import { BadgeCheckIcon, ChipIcon } from "@heroicons/react/solid";
+import React from "react";
+import { skills } from "../data";
+
+export default function Skills() {
+  return (
+    <section id="skills">
+      <div className="container px-5 py-10 mx-auto">
+        <div className="text-center mb-20">
+          <ChipIcon className="w-10 inline-block mb-4" />
+          <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
+            Skills &amp; Technologies
+          </h1>
+          <p className="text-base leading-relaxed xl:w-3/4 lg:w-3/4 mx-auto">
+            I firmly believe that the most important skill is that of being able to solve problems. 
+            <br />I also fundamentally believe in my ability to quickly learn new programming languages and concepts as the need and desire presents itself.
+           <br /> However, below are the technologies in which I have acquired the most experience and would be most confident in applying immediately, through bith self-learning and educating others.
+          </p>
+        </div>
+        <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+          {skills.map((skill) => (
+            <div key={skill} className="p-2 sm:w-1/2 w-full">
+              <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+                <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
+                <span className="title-font font-medium text-white">
+                  {skill}
+                </span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const projects = [
+  {
+    title: "Theberton Lion",
+    subtitle: "MERN Stack",
+    description:
+      "Live website of the Theberton Lion Public House, Suffolk, England",
+    image: "./project-1.gif",
+    link: "https://thebertonlion.co.uk",
+    github: "https://github.com/avidler/theberton-lion-v3#readme",
+  },
+  {
+    title: "Typing Memory Game",
+    subtitle: "MERN Stack",
+    description:
+      "A memory game I created to practice Javascript and React",
+    image: "./project-2.gif",
+    link: "https://avidler-typing-memory-game-v4.herokuapp.com/",
+    github: "https://github.com/avidler/typing-gameV4#readme",
+  },
+
+];
+
+export const testimonials = [
+ 
+
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Express",
+  "Node",
+  "Tailwind CSS",
+  "HTML",
+  "Python",
+  "SQL",
+];
